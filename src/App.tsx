2e3b3d4b13e67f582fbe90fd160e8a3c007353b6
Sofia Sabrina Silva <sofiasabrinasilva@gmail.com>
2026-04-06 00:26:54 -0300

    ✨ New Feature: Construção da página de Cadastro/Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home'
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar />
-    <Home />
+    <div className="min-h-[80vh]">
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Cadastro />} />
+      </Routes>
+    </div>
     <Footer />
+    </BrowserRouter>
     </>
   );
 }

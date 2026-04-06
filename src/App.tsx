50ad8ecd71dcf506fbc65cb7bd10694efefe4dcb
Sofia Sabrina Silva <sofiasabrinasilva@gmail.com>
2026-04-06 00:56:24 -0300

    ✨ New Feature: Construção dos Componentes Navbar e Footer. Adição das rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
     <Navbar />
     <div className="min-h-[80vh]">
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Cadastro />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </div>
     <Footer />
